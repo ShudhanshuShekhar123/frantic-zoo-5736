28f6dbc911e77f58b3222b6361eae314a1d3b24b
shudhanshuShekhar <shudhanshuraz01@gmail.com>
2023-03-30 23:40:12 +0530

    adding

---
+++ my-app/src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+import Homepage from "./Pages/Homepage";
+
+function App() {
+  return (
+    <div className="App">
+      {/* Do not change anything here */}
+      <Homepage />
+    </div>
+  );
+}
+
+export default App;
---
+++ my-app/src/Pages/Homepage.jsx
@@ -0,0 +1,8 @@
+const Homepage = ()=>{
+
+    return (
+        <h1>this is home page</h1>
+    )
+}
+
+export default Homepage
